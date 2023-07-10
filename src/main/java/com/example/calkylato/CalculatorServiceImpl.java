115a33c82e0d3e4196c2a7b7805558c1bd594c84
РЕМЭКС <glotov-aleks@mail.ru>
2023-07-10 18:36:21 +0500

    spring

---
+++ src/main/java/com/example/calkylato/CalculatorServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.calkylato;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Service
+public  class CalculatorServiceImpl implements CalculatorService {
+    public String helloCalc() {
+        return "Добро пожаловать в калькулятор";
+    }
+
+    public int sum( int num1, int num2) {
+
+        return num1+num2;
+    }
+
+    public int min(int num1, int num2) {
+
+        return num1-num2;
+    }
+    public int multiply(int num1, int num2){
+        return num1*num2;
+
+    }
+    public double divide(int num1, int num2) {
+        return (double) num1 / num2;
+    }
+}
+
